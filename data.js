65ac0ec446a93675ceaeb55efdf2e61fdc52ef0f
Be <bebirchall@gmail.com>
2014-02-04 10:14:15 -0500

    some styling

--- data.js
+++ data.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
 
 // stuff that would be determied on back end in node version
+
+
+
 	var source = $("#revisions-template").html();
 	var template = Handlebars.compile(source);
 
-	var rows = [{commitHash: '58sl384jd', timestamp: '3 January 2014', numtests: 1203},
-				{commitHash: 's9d8dks7e', timestamp: '12 January 2014', numtests: 2938},
-				{commitHash: 'sldf8293m', timestamp: '18 December 2013', numtests: 120392},
-				{commitHash: 'b39clskd9', timestamp: '15 December 2013', numtests: 130293}
+
+
+	Handlebars.registerHelper('formatHash', function(hash){
+		return hash.slice(0,10);
+	});
+
+	Handlebars.registerHelper('formatDate', function(timestamp){
+		return timestamp.slice(4,21);
+	});
+
+	var rows = [{commitHash: '7c66af17c773bfcebe32583b9e9b1e03b9232dc8', timestamp: 'Fri Jan 31 2014 23:54:34 GMT+0000 (UTC)', numtests: 159385},
+				{commitHash: '023ac3f0f4fa91a86d932189f771a7eda2c6093e', timestamp: 'Wed Jan 29 2014 17:57:29 GMT+0000 (UTC)', numtests: 159388},
+				{commitHash: '344bf6593bb6624d7cf62673f7b861ab0fb5bfb5', timestamp: 'Fri Jan 17 2014 22:14:46 GMT+0000 (UTC)', numtests: 159407},
+				{commitHash: '42396f8c977169641f76e0c23e0d84e0fad780c0', timestamp: 'Fri Jan 17 2014 00:17:55 GMT+0000 (UTC)', numtests: 159408},
+				{commitHash: 'cd85d32280f63e14e95fd303d2f47eba3e9f1987', timestamp: 'Thu Jan 16 2014 02:26:56 GMT+0000 (UTC)', numtests: 159411},
+				{commitHash: '5db8e91b576174f3c107a95bfe18e699ede87e61', timestamp: 'Wed Jan 08 2014 18:36:34 GMT+0000 (UTC)', numtests: 159383}
 	];
 
 	var revisions = [];
@@ -22,7 +37,7 @@ $(document).ready(function(){
 	}
 
 	var data = {
-		cutoff: '15 December, 2013',
+		cutoff: '8 January 2014',
 		revision: revisions
 	};
 	var html = template(data);
@@ -41,14 +56,16 @@ $(document).ready(function(){
 		if (selected.hash === hash) {
 			selected = {hash: null, index: null};
 			$(this).find('.compare').show();
+			$(this).children('.hash').css('color', '');
 			$(this).css('background', '');
 			$('li').last().find('.compare').hide();
 			$('li').each(function(){
-				$(this).find('span').text('next');
+				$(this).find('.otherRev').text('previous revision');
 				var nextHash = $(this).next().attr('commit');
 				var thisHash = $(this).attr('commit');
 				$(this).find('.reg').children('a').attr('href', '/regressions/between/' + nextHash + '/' + thisHash);
-				$(this).find('.fix').children('a').attr('href', '/fixes/between/' + nextHash + '/' + thisHash);			});
+				$(this).find('.fix').children('a').attr('href', '/fixes/between/' + nextHash + '/' + thisHash);
+			});
 		} else {
 			selected = {hash: hash, index: $(this).index()};
 			$('li').last().find('.compare').show();
@@ -60,14 +77,14 @@ $(document).ready(function(){
 				$(this).find('.fix').children('a').attr('href', '/fixes/between/' + oldHash + '/' + newHash);
 				$(this).css('background', '');
 				$(this).find('.compare').show();
-				$(this).find('span').text('selected');
+				$(this).find('.otherRev').html('revision <span class=hash>' + selected.hash.slice(0,10) + '</span>');
+				$(this).children('.hash').css('color', '');
 			});
-			$(this).css('background', 'rgba(65,131,196,0.5)');
+			$(this).css('background', 'rgba(252, 252, 226, 1)');
+			$(this).children('.hash').css('color', 'rgb(65,131,196)');
 			$(this).find('.compare').hide();
 		}
 	});
-
-
 
 });
 
